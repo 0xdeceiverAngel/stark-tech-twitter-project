d9d3ba875379f50c07ef3d6a509ef713f4e45fff
0xdeciverAngel <31663805+0xdeciverAngel@users.noreply.github.com>
2021-04-08 23:19:53 +0800

    finish api

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ var tweet_model_data = [{
     "id": 0,
     "title": "hello",
     "content": "hello",
-    "user": [1]
+    "user": 1
 }];
+var current_tweet_len = 1;
 // var jsonData = JSON.stringify(users);
 // fs.writeFile("test.txt", jsonData, function(err) {
 //     if (err) {
@@ -52,7 +53,7 @@ const typeDefs = gql `
       id: ID!
       title: String
       content: String
-      user: [ID]
+      user: ID
   }
   type Query {
     list_all_user: [user_model] 
@@ -61,10 +62,10 @@ const typeDefs = gql `
     list_tweet(id:ID!):[tweet_model]
     test:String
   }
-#   type Mutation{
-#     write_tweet(input:)
-#     follow_a_user()
-#     }
+  type Mutation{
+    write_tweet(title:String!,content:String!,user:ID!):String
+    follow_a_user(active_id: ID!, passive_id: ID!): String
+    }
 `;
 const resolvers = {
     Query: {
@@ -95,6 +96,51 @@ const resolvers = {
         test: () => 'im test'
 
     },
+    Mutation: {
+        write_tweet: (root, args, context) => {
+            const {
+                id,
+                title,
+                content,
+                user
+            } = args;
+            // console.log(args);
+            tweet_model_data.push({
+                id: current_tweet_len,
+                title: title,
+                content: content,
+                user: parseInt(user, 10)
+            });
+            // console.log(tweet_model_data);
+            var res = user_model_data.filter(function(item) {
+                if (item.id == user) {
+                    return 'mark';
+                }
+            });
+            res[0].tweeets.push(parseInt(id, 10));
+            current_tweet_len = current_tweet_len + 1;
+
+            return 'ok';
+        },
+        follow_a_user: (root, args, context) => {
+            const {
+                active_id,
+                passive_id
+            } = args;
+            var res = user_model_data.filter(function(item) {
+                if (item.id == passive_id) {
+                    return 'mark';
+                }
+            });
+            // console.log(res);
+            id = parseInt(active_id, 10)
+            if (!res[0].followers.includes(id)) {
+                res[0].followers.push(id);
+
+            }
+            return 'ok';
+        },
+    }
 
 };
 
